86d5d1914b74c4f69f3990abcf6df2d1e979f0bf
Valentin <gribvalentin@outlook.com>
2019-09-17 22:36:18 +0100

    new solutions

--- src/medium/friend_circle_queries/Main.kt
+++ src/medium/friend_circle_queries/Main.kt
@@ -1,45 +1,51 @@
 package medium.friend_circle_queries
-
 import java.util.*
+
+
 // TODO: WORK IN PROGRESS
-class FriendCircles {
-    private val list = mutableMapOf<Int, Int>()
-    private val nodesCount = mutableMapOf<Int, Int>()
-    val rank = mutableMapOf<Int, Int>()
+class FriendCircles(values: List<Int>) {
+    private val list: MutableMap<Int, Int> = values.map { it to it }.toMap().toMutableMap()
+    private val nodesCount: MutableMap<Int, Int> = values.map { it to 1 }.toMap().toMutableMap()
+    private val rank: MutableMap<Int, Int> = values.map { it to 1 }.toMap().toMutableMap()
+    var max = 0
+        private set
 
     private fun findRoot(i: Int): Int {
         val parent = list[i]!!
+
         return if (parent == i) i else {
             val result = findRoot(parent)
-            list[i] = result
+            // list[i] = result
             result
         }
     }
-    fun getMaxNode(): Int{
-        return nodesCount.values.max()!!
-    }
+
 
     fun addQuery(query: Array<Int>) {
         val a = query[0]
         val b = query[1]
-        // If it's a new node we initialize it
-        list.getOrPut(a) { a }
-        list.getOrPut(b) { b }
-        nodesCount.getOrPut(a) { 1 }
-        nodesCount.getOrPut(b) { 1 }
 
         val rootA = findRoot(a)
         val rootB = findRoot(b)
 
+        val rankA = rank.getValue(rootA)
+        val rankB = rank.getValue(rootB)
+
+        val mainNode = if (rankA < rankB) rootB else rootA
+        val smallerNode = if (rankA < rankB) rootA else rootB
+
         if (rootA != rootB) {
-            list[rootA] = list.getValue(rootB)
-            nodesCount[rootB] = nodesCount.getValue(rootB) + nodesCount.getValue(rootA)
-            nodesCount.remove(rootA)
+            // we add rootA to rootB
+            list[smallerNode] = list.getValue(mainNode)
+            nodesCount[mainNode] = nodesCount.getValue(mainNode) + nodesCount.getValue(smallerNode)
+            // set max if necessary
+            max = if (nodesCount[mainNode]!! > max) nodesCount[mainNode]!! else max
+            if (rankA == rankB)
+                rank[rootA] = rankA + 1
         }
     }
 
 }
-
 
 
 fun main() {
@@ -52,11 +58,14 @@ fun main() {
     for (i in 0 until q) {
         queries[i] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
     }
-    val friendCircles = FriendCircles()
+    val allData = queries.flatten().distinct()
+    val friendCircles = FriendCircles(allData)
     val maxArr = mutableListOf<Int>()
-    for (query in queries){
+
+    for (query in queries) {
         friendCircles.addQuery(query)
-        maxArr.add(friendCircles.getMaxNode())
+        maxArr.add(friendCircles.max)
     }
+
     println(maxArr.joinToString("\n"))
 }
